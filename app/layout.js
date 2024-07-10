dd1f79846a09c1be38d812dbfbe70220311e1d78
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-07-10 18:44:42 +0100

    favcion has been added

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const pathName = usePathname();
-  console.log(pathName);
+
   var data = "";
   switch (pathName) {
     case "/":
@@ -35,6 +35,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>{data}</title>
       </head>
       <body className={inter.className}>
